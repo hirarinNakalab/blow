5987b7c1172c503a90a4490091b431bd75becd4d
nakaLab <hirakawa.rin151@mail.kyutech.jp>
2020-04-21 17:43:06 +0900

    change tsm reader

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,14 +1,22 @@
-from audiotsm import phasevocoder
-from audiotsm.io.wav import WavReader, WavWriter
+from audiotsm import wsola
+from audiotsm.io.array import ArrayReader, ArrayWriter
 
 import os
 import glob
+import librosa
+import numpy as np
 
 def time_stretch(input_fn, output_fn, speed):
-    with WavReader(input_fn) as reader:
-        with WavWriter(output_fn, reader.channels, reader.samplerate) as writer:
-            tsm = phasevocoder(reader.channels, speed=speed)
-            tsm.run(reader, writer)
+    data, sr = librosa.load(input_fn)
+    data = data.reshape(1, -1)
+    reader = ArrayReader(data)
+    writer = ArrayWriter(channels=1)
+    tsm = wsola(channels=1, speed=speed)
+    tsm.run(reader, writer)
+    output = np.ascontiguousarray(writer.data.T)
+    output = output.flatten()
+    librosa.output.write_wav(output_fn, output, sr=22050)
+
 
 if __name__ == '__main__':
     base = './wav'
