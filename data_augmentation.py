04c61dd7bf98347386dd0c15b8a1438b9f06c761
nakaLab <hirakawa.rin151@mail.kyutech.jp>
2020-04-23 10:19:25 +0900

    add argparse to data_augmentation.py

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,3 +1,5 @@
+import argparse
+
 from audiotsm import wsola
 from audiotsm.io.array import ArrayReader, ArrayWriter
 
@@ -5,8 +7,7 @@ import os
 import glob
 import librosa
 import numpy as np
-import shutil
-
+import sys
 
 def time_stretch(data, speed):
     data = data.reshape(1, -1)
@@ -21,35 +22,39 @@ def time_stretch(data, speed):
 def pitch_shift(data, n_steps):
     return librosa.effects.pitch_shift(data, 22050, n_steps=n_steps)
 
-def frame_shift(data, n_shift, sr=22050):
-    n_frame_div = 5
-    frame_length = 5 # [ms]
-    shift_sample = int(sr * (frame_length/1000) / n_frame_div)
-    head = np.zeros(shift_sample*n_shift)
-    tail = np.zeros(shift_sample*(n_frame_div-n_shift))
-    data = np.concatenate([head, data, tail])
-    return data
+# def frame_shift(data, n_shift, sr=22050):
+#     n_frame_div = 5
+#     frame_length = 5 # [ms]
+#     shift_sample = int(sr * (frame_length/1000) / n_frame_div)
+#     head = np.zeros(shift_sample*n_shift)
+#     tail = np.zeros(shift_sample*(n_frame_div-n_shift))
+#     data = np.concatenate([head, data, tail])
+#     return data
 
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Data augmentation script')
+    parser.add_argument('--n_pitch_shift', default=3, type=int, required=True, help='(default=%(default)s)')
+    parser.add_argument('--n_stretch', default=3, type=int, required=True, help='(default=%(default)s)')
+    args = parser.parse_args()
+
     base = './wav'
     search_path = f"{base}_bck/*.wav"
 
     for file in glob.glob(search_path):
         fn = os.path.basename(file)
-        shutil.copyfile(file, f"{base}/{fn}")
         fn = fn.replace(".wav", "").split("_")
         pattern, num = '_'.join(fn[:-1]), fn[-1]
         data, sr = librosa.load(file)
 
-        for steps in np.linspace(-0.125, 125, 3):
+        for steps in np.linspace(-0.125, 125, args.n_pitch_shift):
             pitched = pitch_shift(data, steps)
             p_shift = f"({steps})pitch"
             if "noised" in file:
-                for i in np.linspace(0.98, 1.02, 3):
-                    stretched = time_stretch(pitched, i)
-                    n_stretch = f"{i}stretched"
+                for n in np.linspace(0.98, 1.02, args.n_stretch):
+                    stretched = time_stretch(pitched, n)
+                    n_stretch = f"{n}stretched"
 
                     output_fn = f"{base}/{pattern}_{p_shift}-{n_stretch}-{num}.wav"
                     librosa.output.write_wav(output_fn, stretched, sr=22050)
--- mel_to_audio.py
+++ mel_to_audio.py
@@ -1,9 +1,9 @@
 import glob
 import os
-
 import librosa
 import numpy as np
 import zipfile
+import shutil
 
 
 if __name__ == '__main__':
@@ -13,8 +13,10 @@ if __name__ == '__main__':
         zf.extractall(extract_path)
 
     output_path = "./wav"
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
+    bck_path = "./wav_bck"
+    for path in [output_path, bck_path]:
+        if not os.path.exists(path):
+            os.mkdir(path)
 
     for i, file in enumerate(glob.glob(f"{extract_path}/*/*.npy")):
         mel = np.load(file)
@@ -23,3 +25,7 @@ if __name__ == '__main__':
         wav_filepath = f"{output_path}/{filename}.wav"
         librosa.output.write_wav(wav_filepath, audio, sr=22050, norm=False)
         print(f"save converted wav to {wav_filepath}.")
+
+    for file in glob.glob(f"{output_path}/*.wav"):
+        fn = os.path.basename(file)
+        shutil.copyfile(file, f"{bck_path}/fn")
