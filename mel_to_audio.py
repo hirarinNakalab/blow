be44652e85cc58a729d08335e0adb1d3f551f777
nakaLab <hirakawa.rin151@mail.kyutech.jp>
2020-04-23 11:21:43 +0900

    fix

--- mel_to_audio.py
+++ mel_to_audio.py
@@ -18,13 +18,13 @@ if __name__ == '__main__':
         if not os.path.exists(path):
             os.mkdir(path)
 
-    # for i, file in enumerate(glob.glob(f"{extract_path}/*/*.npy")):
-    #     mel = np.load(file)
-    #     audio = librosa.feature.inverse.mel_to_audio(mel)
-    #     filename = os.path.basename(file).replace(".npy", "")
-    #     wav_filepath = f"{output_path}/{filename}.wav"
-    #     librosa.output.write_wav(wav_filepath, audio, sr=22050, norm=False)
-    #     print(f"save converted wav to {wav_filepath}.")
+    for i, file in enumerate(glob.glob(f"{extract_path}/*/*.npy")):
+        mel = np.load(file)
+        audio = librosa.feature.inverse.mel_to_audio(mel)
+        filename = os.path.basename(file).replace(".npy", "")
+        wav_filepath = f"{output_path}/{filename}.wav"
+        librosa.output.write_wav(wav_filepath, audio, sr=22050, norm=False)
+        print(f"save converted wav to {wav_filepath}.")
 
     for file in glob.glob(f"{output_path}/*.wav"):
         fn = os.path.basename(file)
